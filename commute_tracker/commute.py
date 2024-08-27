76972aff895c4938f193592edbeeda882055d4f1
nico <ngalfond@ercbroadband.com>
2024-08-27 14:41:58 -0400

    initial functionality

---
+++ commute_tracker/commute.py
@@ -0,0 +1,35 @@
+import googlemaps
+import pprint
+import statistics
+import csv
+
+gmaps = googlemaps.Client(key='AIzaSyDXr0gqy7jYfQG_aq20IHrbpu2zgxs3oaI')
+
+work_location = 35.83158882391046, -82.67251414517058
+
+home_location = 35.59424863065563, -82.55800273168425
+
+
+
+
+full_matrix = gmaps.directions(home_location, work_location)
+
+#pprint.pprint(full_matrix[0]["legs"][0]["duration"]["text"])
+
+trip_time = (full_matrix[0]["legs"][0]["duration"]["text"])
+
+
+print(f'The current estimated commute to work will take {trip_time}.')
+
+time_int = int(trip_time.split()[0])
+
+
+with open('trips.csv', 'r+') as t:
+    all_times = t.read().split(',')[:-1]
+    t.write(f"{time_int},", )
+
+int_times = map(int, all_times)
+
+print(all_times)
+
+print(statistics.mean(int_times))
